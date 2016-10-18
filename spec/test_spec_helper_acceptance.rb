472aca74b486b48b1eb4b7da711874f207ce2460
Travis N. Thomsen <tthomsen@binaryvoid.com>
2016-10-18 13:15:31 -0500

    add code to install puppet

--- spec/test_spec_helper_acceptance.rb
+++ spec/test_spec_helper_acceptance.rb
@@ -6,6 +6,38 @@ run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
 master_fqdn = "test.beacker.vm"
 master = only_host_with_role(hosts, 'master')
 
+hosts.each do |host|
+  if host['roles'].include?('master')
+    on master, "yum -y install puppet-server"
+    on master, "echo '*' > /etc/puppet/autosign.conf"
+    on master, "mkdir -p /etc/puppet/hiera"
+
+    config = {
+      'main' => {
+        'server'   => master_fqdn,
+        'certname' => master_fqdn,
+        'logdir'   => '/var/log/puppet',
+        'vardir'   => '/var/lib/puppet',
+        'ssldir'   => '/var/lib/puppet/ssl',
+        'rundir'   => '/var/run/puppet'
+      },
+      'agent' => {
+        'certname' => master_fqdn,
+        'classfile'   => '$vardir/classes.txt',
+        'localconfig' => '$vardir/localconfig',
+        'environment' => 'production',
+        'pluginsync'  => 'true',
+        'masterport'  => '8140'
+      },
+      'master' => {
+        'pluginsync' => 'true'
+      }
+    }
+
+    configure_puppet_on(master, config)
+  end
+end
+
 RSpec.configure do |c|
   puts "RSpec configure"
   # Project root
